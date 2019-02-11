ab85cecdbecb1f76509983666d63a7ed3846d820
shakiblimon <shakiblimon@circlefintech.com>
2019-02-11 13:38:23 +0600

    Initial commit

---
+++ list.py
@@ -0,0 +1,19 @@
+__version__ = "V1.0.0"
+__author__ = "Shakib Limon"
+
+'''
+|   Single Linked List
+
+A single linked list contains an item and reference to the next item .
+
+'''
+
+#   Creating  Node Class
+
+class Node :
+    def __init__(self,data):
+        self.item = data
+        self.ref = None
+
+
+
