fe07a6bc4e164a4320a80e8fb608f369074cc718
shakiblimon <shakiblimon@circlefintech.com>
2019-02-24 14:10:54 +0600

    New Window segment newly added and updated

--- GUI/gui_dev.py
+++ GUI/gui_dev.py
@@ -4,6 +4,7 @@ root = tkinter.Tk()
 #title bar name
 root.title('Test App')
 
+
 # Resize the window
 root.resizable(width="false", height="false")
 
@@ -15,20 +16,26 @@ root.maxsize(width=300, height=300)
 '''
         Frame
 '''
-frame_labels = tkinter.Frame(root, borderwidth="2", relief="ridge")
+frame_labels = tkinter.Frame(root, relief="ridge")
 frame_labels.grid(column=0, row=0, sticky="ns")
 
 
-frame_buttons = tkinter.Frame(root, borderwidth="2", relief="ridge")
+frame_buttons = tkinter.Frame(root, relief="ridge")
 frame_buttons.grid(column=1, row=0)
 
+'''
+        New Window
+'''
+new_window = tkinter.Toplevel()
+new_window.withdraw()
 
 simple_label = tkinter.Label(root, text="Hello ! Whats up?")
-another_label = tkinter.Label(root , text = "More Text")    #   Widget Orientation
+another_label = tkinter.Label(root, text="More Text")    #   Widget Orientation
 
 
-close_button = tkinter.Button(root, text ="Close", command =root.destroy)
-another_button = tkinter.Button(root, text = "Do Nothing")  #   Widget Orientation
+close_button = tkinter.Button(frame_buttons, text="Close", command=root.destroy)
+another_button = tkinter.Button(frame_buttons, text="Do Nothing")  #  Widget Orientation
+window_button = tkinter.Button(frame_buttons, text="Show new window", command=new_window.deiconify) #New Window added
 
 
 ######  Widget Orientation   #########
@@ -36,14 +43,14 @@ simple_label.grid(column=0, row=0, sticky="ew")
 another_label.grid(column=0, row=1, sticky="ew")
 
 close_button.grid(column=1, row=0, sticky="ew")
-another_button.grid(column=1, row=1 , sticky="ew")
+another_button.grid(column=1, row=1, sticky="ew")
 
 
 #
 # simple_label.pack()
 # another_label.pack()
 #
-# # close_button.pack()
-# # another_button.pack()
+# close_button.pack(fill ="x")
+# another_button.pack(fill ="x")
 
 root.mainloop()
