9bd11b555deaa9494156fd7ee74fd63003006cb7
Shakib Limon <shakib.limon@circlefintech.com>
2019-02-25 17:56:49 +0600

    Fibonacci Progression Class added

--- DSA/inheritance.py
+++ DSA/inheritance.py
@@ -38,3 +38,17 @@ class GeometricProgression(Prograssion):
 
     def _advance(self):
         self._current *=self._base
+
+
+'''
+        A Fibonacci Progression Class
+'''
+
+class FibonacciProgression(Prograssion):
+    def __init__(self, first=0, second =1):
+        super().__init__(first)
+        self._prev = second-first
+
+
+    def _advance(self):
+        self._prev, self._current = self._current, self._prev+ self._current
