4001da5a3c65a057ad1fb3af1f0c921340612b3b
Shakib Limon <shakib.limon@circlefintech.com>
2019-03-14 11:12:37 +0600

    Socket segment added

---
+++ socket/create.py
@@ -0,0 +1,15 @@
+__version__ ="1.0.0"
+__author__ ="Shakib Limon"
+
+import socket
+#Decalre a socket variable
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind('localhost',80000)
+s.listen(1)
+conn = addr = s.accept()
+while 1:
+    data = conn.recv(1024)
+    if not  data:
+        break
+    conn.sendall(data)
+conn.close()
