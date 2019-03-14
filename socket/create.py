35ba9ec4e40697c81796201e3d90db622f73ccce
Shakib Limon <shakib.limon@circlefintech.com>
2019-03-14 12:49:51 +0600

    connecting to google server

--- socket/create.py
+++ socket/create.py
@@ -2,18 +2,28 @@ __version__ ="1.0.0"
 __author__ ="Shakib Limon"
 
 import socket
+import sys
 #Decalre a socket variable
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind('localhost',80000)
-# s.listen(1)
-# conn = addr = s.accept()
-# while 1:
-#     data = conn.recv(1024)
-#     if not  data:
-#         break
-#     conn.sendall(data)
-# conn.close()
-#ip = socket.gethostbyaddr(host='wwww.python.org')
-host = socket.getfqdn('www.google.com')
-#print(ip)
-print(host)
+
+'''
+|           Conneting to server
+'''
+# ip = socket.gethostbyname('www.google.com')
+# print('ip address is :', ip)
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket successfully created')
+
+except socket.error as err:
+    print('socket creation failed with error %s'%(err))
+
+port=56
+try:
+    host_ip= socket.gethostbyname('www.google.com')
+    #host_ip = socket.gethostbyaddr('172.217.194.104')
+except socket.gaierror:
+    print('there is en error resolving the host')
+
+s.connect(host_ip,port)
+print('the socket has successfully connected to google \ on port == %s'% (host_ip))
--- socket/socket.md
+++ socket/socket.md
@@ -6,5 +6,27 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ```
 *_AF_INET_* refers to the address family ipv4.<br>
-*_SOCK_STREAM_* means connection oriented TCP protocol. 
+*_SOCK_STREAM_* means connection oriented TCP protocol.
+
+#### Conneting to server
+**Using Ip Address**
+
+Let's check ip address first
+```shell
+$ping www.google.com
+```
+Also can find the ip using python:
+```python
+ip = socket.gethostbyname('www.=google.com')
+print('ip address is :', ip)
+```
+> ip address is : 74.125.24.105
+
+**Now connecting to google server**
+```python
+
+```
+
+
+
 
