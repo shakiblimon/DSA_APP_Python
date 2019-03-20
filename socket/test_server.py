f8542c877355bb74e8676b5e12af4520d4bfce2a
Shakib Limon <shakib.limon@circlefintech.com>
2019-03-20 18:08:50 +0600

    updated

--- socket/test_server.py
+++ socket/test_server.py
@@ -1,23 +1,65 @@
-import engineio
-import eventlet
+__author__ ='Shakib Limon'
+__version__ = '1.0.0'
+
+# import eventlet
+# import socketio
+#
+# sio = socketio.Server()
+# app = socketio.WSGIApp(sio,static_files={
+#     '/':{'content_type': 'text/html', 'filename':'index.html'}
+# })
+#
+# @sio.on('connect')
+# def connect(sid, environ):
+#     print('connect', sid)
+#
+# @sio.on('my message')
+# def message(sid, data):
+#     print('message', data)
+#
+# @sio.on('disconnect')
+# def disconnet(sid):
+#     print('disconnect',sid)
+#
+# if __name__ == '__main__':
+#     eventlet.wsgi.server(eventlet.listen(('',5000)),app)
+
+from aiohttp import web
 import socketio
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio,static_files={
-    '/':{'content_type': 'text/html', 'filename':'index.html'}
-})
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
 
-@sio.on('connect')
-def connect(sid, environ):
-    print('connect', sid)
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-@sio.on('my message')
-def message(sid, data):
-    print('message', data)
+# @sio.on('connect', namespace='/chat')
+# def connect(sid, environ):
+#     print("connect ", sid)
+#
+# @sio.on('chat message', namespace='/chat')
+# async def message(sid, data):
+#     print("message ", data)
+#     await sio.emit('reply', room=sid)
+#
+# @sio.on('disconnect', namespace='/chat')
+# def disconnect(sid):
+#     print('disconnect ', sid)
+@sio.on('message')
+async def print_message(sid, message):
+    # When we receive a new event of type
+    # 'message' through a socket.io connection
+    # we print the socket ID and the message
+    print("Socket ID: " , sid)
+    print(message)
+    await sio.emit('message', message[::-1])
 
-@sio.on('disconnect')
-def disconnet(sid):
-    print('disconnect',sid)
+
+# app.router.add_static('/static', 'static')
+app.router.add_get('/', index)
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('',5000)),app)
+    web.run_app(app)
--- socket/test_socket.py
+++ socket/test_socket.py
@@ -7,12 +7,12 @@ sio = socketio.Client()
 
 @sio.on('connect')
 def on_connect():
-    print('conneted')
+    print('connection established')
 
 @sio.on('my message')
 def on_message(data):
     print('message received with ', data)
-    sio.emit('my response',{'response': 'my response'})
+    sio.emit('my message', {'foo': 'bar'})
 
 @sio.on('disconnect')
 def on_disconnect():
